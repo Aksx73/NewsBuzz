9e29e93f20f98b020e545dd916d5afb85c244c74
Akshay <akshaynighot73@gmail.com>
2022-08-09 13:56:46 +0530

    API test added

--- app/src/test/java/com/absut/newsapiclient/data/api/NewsAPIServiceTest.kt
+++ app/src/test/java/com/absut/newsapiclient/data/api/NewsAPIServiceTest.kt
@@ -13,16 +13,18 @@ import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
 class NewsAPIServiceTest {
+
     private lateinit var service: NewsAPIService
-    private lateinit var server: MockWebServer
+    private lateinit var mockServer: MockWebServer
+
 
     @Before
     fun setUp() {
-        server = MockWebServer()
-        service = Retrofit.Builder().baseUrl(server.url(""))
-            .addConverterFactory(GsonConverterFactory.create()).build()
+        mockServer = MockWebServer()
+        service = Retrofit.Builder().baseUrl(mockServer.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
             .create(NewsAPIService::class.java)
     }
 
@@ -32,50 +34,49 @@ class NewsAPIServiceTest {
         val source = inputStream.source().buffer()
         val mockResponse = MockResponse()
         mockResponse.setBody(source.readString(Charsets.UTF_8))
-        server.enqueue(mockResponse)
+        mockServer.enqueue(mockResponse)
     }
 
 
     @Test
-    fun getTopHeadlines_sentRequest_receiveExpected() {
+    fun getTopHeadlinesTest() {
         runBlocking {
             enqueueMockResponse("newsresponse.json")
             val response = service.getTopHeadlinesByCountry("us", 1).body()
-            val request = server.takeRequest()
+            val request = mockServer.takeRequest()
             Truth.assertThat(response).isNotNull()
             Truth.assertThat(request.path).isEqualTo("/v2/top-headlines?country=us&page=1&apiKey=b51a0781fcbf499b96d779d36f0aee46")
         }
     }
 
     @Test
-    fun getTopHeadlines_receivedResponse_correctPageSize(){
+    fun getTopHeadlinesCorrectPageSizeTest(){
         runBlocking {
             enqueueMockResponse("newsresponse.json")
             val response = service.getTopHeadlinesByCountry("us", 1).body()
-            val articles = response!!.articleDtos
+            val articles = response!!.articles
             
             Truth.assertThat(articles.size).isEqualTo(20)
         }
     }
 
     @Test
-    fun getTopHeadlines_receivedResponse_correctContent(){
+    fun getTopHeadlinesCorrectContentTest(){
         runBlocking {
             enqueueMockResponse("newsresponse.json")
             val response = service.getTopHeadlinesByCountry("us", 1).body()
-            val articles = response!!.articleDtos
+            val articles = response!!.articles
             val article = articles[0]
             Truth.assertThat(article.author).isEqualTo("Bryan Pietsch")
             Truth.assertThat(article.url).isEqualTo("https://www.washingtonpost.com/nation/2022/07/24/iowa-campground-shooting-schmidt-family/")
             Truth.assertThat(article.publishedAt).isEqualTo("2022-07-24T12:45:45Z")
-
         }
     }
 
 
     @After
     fun tearDown() {
-        server.shutdown()
+        mockServer.shutdown()
     }
 
 
